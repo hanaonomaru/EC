97da34ae4a76ddcd8e53e09f304a8537f12f71e3
hanaonomaru <hanao.n1227@gmail.com>
2021-06-19 15:31:03 +0900

    test/models/create_data_test.rbを削除した

--- test/models/create_data_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class CreateDateTest < ActiveSupport::TestCase
-    test "正しくデータが生成される" do
-        assert User.create(email: "hp@example.com", password: "hphphp", admin: false)       # ()の中身は自分で考えてみて
-        assert User.create(email: "ad@example.com", password: "adadad", admin: true)        # ()の中身は自分で考えてみて
-        
-    end
-end
