92a7ff62bfddc016d9f7d47749f10999faf61fe5
hanaonomaru <hanao.n1227@gmail.com>
2021-06-19 13:01:40 +0900

    質問：データ生成のテストのエラー内容がわからない

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_082845) do
+ActiveRecord::Schema.define(version: 2021_06_19_032500) do
+
+  create_table "orders", force: :cascade do |t|
+    t.string "product", null: false
+    t.integer "price", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_06_16_082845) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "users"
 end
--- test/models/create_data_test.rb
+++ test/models/create_data_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class CreateDateTest < ActiveSupport::TestCase
     test "正しくデータが生成される" do
-        assert User.create(email: "hp@example.com", password: "hphphp", admin: false)       # ()の中身は自分で考えてみて
-        assert User.create(email: "ad@example.com", password: "adadad", admin: true)        # ()の中身は自分で考えてみて
-        
+        assert User.create(id: 1, email: "hp@example.com", password: "hphphp", admin: false)       # ()の中身は自分で考えてみて
+        assert User.create(id: 2, email: "ad@example.com", password: "adadad", admin: true)        # ()の中身は自分で考えてみて
+        assert Order.create(product: "靴下", price: 300, user_id: 1)
     end
 end
